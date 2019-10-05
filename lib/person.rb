6198cede42e912eda6fcd0d0361eeccf64790c6f
Charlie Oster <oster.charlie@yahoo.com>
2019-10-05 21:12:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,25 @@
+class Person
+  def name=(name)
+    @name = name 
+end
+  def name
+    @name
+ end
+end
 
+class Person
+  def job=(job)
+    @job = job 
+end
+  def job
+    @job
+    
+  end
+end
+  
+fido = Dog.new 
+fido.name 
+
+snoopy = Dog.new
+snoopy.breed = "Beagle"
+snoopy.breed
